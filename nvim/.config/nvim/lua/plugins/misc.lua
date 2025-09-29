17ff3323871958a8ae316270beb68046526cd83b
divolli <dpavlysh@gmail.com>
2025-09-29 23:44:47 +0200

    tmux with nvim tmux relation conf added

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -45,4 +45,15 @@ return {
 			require("nvim-surround").setup()
 		end,
 	},
+	{
+		-- tmux & nvim relations
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+		keys = {
+			{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to left pane" },
+			{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to down pane" },
+			{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to up pane" },
+			{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to right pane" },
+		},
+	},
 }
